a3f5f0fcc6049884e39185b3989ee96261c515c7
Mayson34 <54995131+Mayson34@users.noreply.github.com>
2020-04-29 12:46:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -73,6 +73,7 @@ class drawArm:
         length0 = self.beamLengths[0]
         length1 = self.beamLengths[1]
         print("FIRST: ")
+        print (self.getAngleShoulder())
 
         print (self.findSlope(self.shoulder[0], self.shoulder[1], self.elbow[0], self.elbow[1]))
         print (self.findSlope(self.elbow[0], self.elbow[1], self.wrist[0], self.wrist[1]))
@@ -97,25 +98,27 @@ class drawArm:
 
         elif any(self.getAngleShoulder() == 180) and self.wrist[1] <= .1 and self.wrist[0] >= 0:
             print ("ELIF 3")
-            self.wrist[0] = .1
-            self.wrist[1] = -.1
+            self.wrist[1] = .1
+            self.wrist[0] = -.1
 
         elif any(self.getAngleShoulder() > 50) and self.elbow[0] < -.5 and self.elbow[1] < 0:
-
+            
             while all(self.getAngleShoulder() > 50):
                 print ("ELIF 4 WHILE 1")
                 print (self.getAngleShoulder())
                 self.elbow[0] = self.elbow[0] + .01
                 self.elbow[1] = self.elbow[1] - .01
-
+                
+               
+                
                 self.wrist = self.elbow + np.array([length1 * cos(theta0 + theta1), length1 * sin(theta0 + theta1)])
                 print (self.angle2)
                 print (self.angle1)
-
-        #'''
+            
+        '''
         counter = 0
-        while self.findSlope(self.elbow[0], self.elbow[1], self.wrist[0], self.wrist[1]) < self.findSlope(self.shoulder[0], self.shoulder[1], self.elbow[0], self.elbow[1]) and self.elbow[1] >= 0 and self.elbow[0] >= 0:
-
+        while self.findSlope(self.elbow[0], self.elbow[1], self.wrist[0], self.wrist[1]) < self.findSlope(self.shoulder[0], self.shoulder[1], self.elbow[0], self.elbow[1]) - 1 and self.elbow[1] >= 0 and self.elbow[0] >= 0:
+            
             self.wrist[0] = self.wrist[0] - .01
             self.wrist[1] = self.wrist[1] + .01
             counter += .01
@@ -125,8 +128,8 @@ class drawArm:
             #self.wrist = self.elbow + np.array([length1 * cos(theta0 + theta1), length1 * sin(theta0 + theta1)])
             print ("FSLOPE1")
         #self.wrist = self.elbow + np.array([length1 * cos(self.theta0 + self.theta1), length1 * sin(self.theta0 + self.theta1)])
-        self.wrist = self.elbow - counter + np.array([length1 , length1])
-        '''
+        #self.wrist = self.elbow - counter + np.array([length1 , length1])
+        
         while self.findSlope(self.elbow[0], self.elbow[1], self.wrist[0], self.wrist[1]) > self.findSlope(self.shoulder[0], self.shoulder[1], self.elbow[0], self.elbow[1]) and self.elbow[1] < 0:
             self.wrist[0] = self.wrist[0] + 1
             self.wrist[1] = self.wrist[1] + .1
@@ -187,8 +190,12 @@ arm = drawArm()
 
 
 theta0 = 1
-theta1 = -1.5
+theta1 = 2 #cannot be less than 0
 
+if theta1 < 0:
+    theta1 = 0
+if theta1 > 3:
+    theta1 = 3
 arm.updateJoints([theta0, theta1])
 
 
