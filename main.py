c5e21d55ea8049c251df7b5ca52812d2422b9fb8
Mayson34 <54995131+Mayson34@users.noreply.github.com>
2020-05-03 18:17:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from math import cos, sin, atan, atan2, atanh
+from math import cos, acos, sin, atan, atan2, atanh
 from math import degrees
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,11 +11,14 @@ class drawArm:
     def __init__(self, jointAngles=[0, 0]):
         self.shoulder = np.array([0, 0])
         self.torso = np.array([(0, 0), (-1, 0)])
-        self.beamLengths = [1, 1]
-        self.updateJoints(jointAngles)
+        self.beamLengths = [2, 2]
+        #self.updateJointsFK(jointAngles)
+        self.updateJointsIK(1, 0)
         self.showAnimation = True
         self.angle1 = 0
         self.angle2 = 0
+        self.T1 = 0
+        self.T0 = 0
 
 
 
@@ -60,11 +63,17 @@ class drawArm:
 
 
 
-    def updateJoints(self, jointAngles):
+    def updateJointsFK(self, jointAngles):
         self.jointAngles = jointAngles
         self.theta0 = self.jointAngles[0]
         self.theta1 = self.jointAngles[1]
         self.forwardKinematics()
+        self.checkAngle()
+
+    def updateJointsIK(self, x, y):
+        x1 = x
+        y1 = y
+        self.inverseKinematics(x1, y1)
         self.checkAngle()
 
     def checkAngle(self):
@@ -80,7 +89,9 @@ class drawArm:
 
         if self.elbow[0] < 0 and self.elbow[1] > 0:
             self.elbow[0] = 0
-            self.elbow[1] = 1
+            self.elbow[1] = 2
+            theta0 = self.T0
+            theta1 = self.T1
             self.wrist = self.elbow + np.array([length1 * cos(theta0 + theta1), length1 * sin(theta0 + theta1)])
             print ("IF")
 
@@ -108,37 +119,14 @@ class drawArm:
                 print (self.getAngleShoulder())
                 self.elbow[0] = self.elbow[0] + .01
                 self.elbow[1] = self.elbow[1] - .01
-                
+                theta0 = self.T0
+                theta1 = self.T1
                
                 
                 self.wrist = self.elbow + np.array([length1 * cos(theta0 + theta1), length1 * sin(theta0 + theta1)])
                 print (self.angle2)
                 print (self.angle1)
             
-        '''
-        counter = 0
-        while self.findSlope(self.elbow[0], self.elbow[1], self.wrist[0], self.wrist[1]) < self.findSlope(self.shoulder[0], self.shoulder[1], self.elbow[0], self.elbow[1]) - 1 and self.elbow[1] >= 0 and self.elbow[0] >= 0:
-            
-            self.wrist[0] = self.wrist[0] - .01
-            self.wrist[1] = self.wrist[1] + .01
-            counter += .01
-            print (self.getAngleElbow())
-            print (self.findSlope(self.shoulder[0], self.shoulder[1], self.elbow[0], self.elbow[1]))
-            print (self.findSlope(self.elbow[0], self.elbow[1], self.wrist[0], self.wrist[1]))
-            #self.wrist = self.elbow + np.array([length1 * cos(theta0 + theta1), length1 * sin(theta0 + theta1)])
-            print ("FSLOPE1")
-        #self.wrist = self.elbow + np.array([length1 * cos(self.theta0 + self.theta1), length1 * sin(self.theta0 + self.theta1)])
-        #self.wrist = self.elbow - counter + np.array([length1 , length1])
-        
-        while self.findSlope(self.elbow[0], self.elbow[1], self.wrist[0], self.wrist[1]) > self.findSlope(self.shoulder[0], self.shoulder[1], self.elbow[0], self.elbow[1]) and self.elbow[1] < 0:
-            self.wrist[0] = self.wrist[0] + 1
-            self.wrist[1] = self.wrist[1] + .1
-
-            print "FSLOPE2"
-            print self.findSlope(self.shoulder[0], self.shoulder[1], self.elbow[0], self.elbow[1])
-            print self.findSlope(self.elbow[0], self.elbow[1], self.wrist[0], self.wrist[1])
-        '''
-        #self.wrist = self.elbow + np.array([length1-.5, length1])
 
     def findSlope(self,x1,y1,x2,y2):
 
@@ -168,7 +156,35 @@ class drawArm:
         self.findAngleShoulder()
         self.findAngleElbow()
 
+    def inverseKinematics(self,x,y):
+        x1 = x
+        y1 = y
+        length0 = self.beamLengths[0]
+        length1 = self.beamLengths[1]
 
+        
+
+        theta1 = acos( ((x1)**2 + (y1)**2 - 2**2 - 2**2) / ( 2 * 2 * 2))
+        theta0 = atan2(y1, x1) - atan2((length1 * sin(theta1)) , (length0 + length1 * cos(theta1)))
+        self.T1 = theta1
+        self.T0 = theta0
+        print(((-x1**2) + (y1**2) -2**2 - 2**2))
+        print(acos(0))
+        print("theta1")
+        print(theta1)
+        print("theta0")
+        print(theta0)
+        if theta1 < 0:
+            theta1 = 0
+        elif theta1 > 3:
+            theta1 = 3
+        self.elbow = self.shoulder + np.array([length0 * cos(theta0), length0 * sin(theta0)])
+        self.wrist = self.elbow + np.array([length1 * cos(theta0 + theta1), length1 * sin(theta0 + theta1)])
+        self.findAngleShoulder()
+        self.findAngleElbow()
+        self.checkAngle()
+        self.findAngleShoulder()
+        self.findAngleElbow()
 
     def plot(self):
 
@@ -187,39 +203,21 @@ class drawArm:
         self.findAngleElbow()
 
 arm = drawArm()
-
-
-theta0 = -1
-theta1 = .5 #cannot be less than 0
-
-wristX = 0
+wristX = 4
 wristY = 0
+'''
+theta0 = -1
+theta1 = 1 #cannot be less than 0
+
+
 if theta1 < 0:
     theta1 = 0
 if theta1 > 3:
     theta1 = 3
-print ("Elbow ")
-print (arm.elbow[0])
-print (arm.elbow[1])
-print ("Wrist ")
-print (arm.wrist[0])
-print (arm.wrist[1])
-'''
-if arm.wrist[0] > wristX:
+    '''
+#arm.updateJointsFK([theta0, theta1])
 
-    while arm.wrist[0] > wristX:
-        theta1 = theta1 +.1
-        print ("Theta1 ")
-        print (theta1)
-        arm.updateJoints([theta0, theta1])
-        '''
-print ("Elbow ")
-print (arm.elbow[0])
-print (arm.elbow[1])
-print ("Wrist ")
-print (arm.wrist[0])
-print (arm.wrist[1])
-arm.updateJoints([theta0, theta1])
+arm.updateJointsIK(wristX, wristY)
 
 
 
@@ -233,7 +231,7 @@ plt.annotate("Wrist", xy=(arm.wrist[0], arm.wrist[1]), xytext=(1, 1.75),
              arrowprops=dict(facecolor='black', shrink=0.05))
 arm.checkAngle()
 
-plt.ylim(-2, 2)
-plt.xlim(-2, 2)
+plt.ylim(-4, 4)
+plt.xlim(-4, 4)
 
 plt.show()
